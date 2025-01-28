9a1b3add47eafe5833b61e8c4ffa3364ec16becd
Witherr <sdssouvic99@gmail.com>
2025-01-29 03:25:14 +0530

    Fix condition in sum50 (thanks jaggu)

--- Assignment 1/3-sum50.c
+++ Assignment 1/3-sum50.c
@@ -6,7 +6,7 @@ const int arraySize = 50;
 
 int main() {
     int sum = 0;
-    for (int i = 1; i < arraySize; i++) {
+    for (int i = 1; i <= arraySize; i++) {
         sum += i;
     }
     printf("The sum of first %d numbers is %d\n", arraySize, sum);
